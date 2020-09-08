07778743e9bcf49328ceae698c0a9a86bca8d0a7
Alfred Chrisdianto Simanjuntak <32379446+PLYAAYSDS@users.noreply.github.com>
2020-09-08 22:37:24 +0700

    Update master.blade.php

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -11,7 +11,7 @@
     <!-- Logo -->
     <a href="{{ url('/') }}" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
-      <span class="logo-mini"><b>T</b>A</span>
+      <span class="logo-mini">LPPM</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>LPPM</b> IT Del</span>
     </a>
